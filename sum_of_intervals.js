b2b263743fda4858c66eeb764a4d9b04114d043e
Dmitry Kanaev <dimmkan@mail.ru>
2023-04-12 08:28:42 +0300

    Sum of Intervals

---
+++ sum_of_intervals.js
@@ -0,0 +1,105 @@
+/**
+ Write a function called sumIntervals/sum_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+
+Intervals
+Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+
+Overlapping Intervals
+List containing overlapping intervals:
+
+[
+   [1, 4],
+   [7, 10],
+   [3, 5]
+]
+The sum of the lengths of these intervals is 7. Since [1, 4] and [3, 5] overlap, we can treat the interval as [1, 5], which has a length of 4.
+
+Examples:
+sumIntervals( [
+   [1, 2],
+   [6, 10],
+   [11, 15]
+] ) => 9
+
+sumIntervals( [
+   [1, 4],
+   [7, 10],
+   [3, 5]
+] ) => 7
+
+sumIntervals( [
+   [1, 5],
+   [10, 20],
+   [1, 6],
+   [16, 19],
+   [5, 11]
+] ) => 19
+
+sumIntervals( [
+   [0, 20],
+   [-100000000, 10],
+   [30, 40]
+] ) => 100000030
+Tests with large intervals
+Your algorithm should be able to handle large intervals. All tested intervals are subsets of the range [-1000000000, 1000000000].
+ */
+
+function sumIntervals(intervals) {
+  const obj = intervals.sort(([start_a], [start_b]) => start_a - start_b).reduce((acc, [start, end]) => {
+    if (start > acc.end) {
+      acc.sum += acc.end - acc.start;
+      acc.start = start;
+      acc.end = end;
+    } else if (start <= acc.end && end > acc.end) {
+      acc.end = end;
+    }
+
+    return acc;
+  }, {
+    sum: 0,
+    start: -1000000001,
+    end: -1000000001,
+  });
+
+  return obj.sum + (obj.end - obj.start);
+}
+
+// Optimize
+// function sumIntervals(xs) {
+//   let ys = xs.sort(([a,b], [c,d]) => a-c);
+//   let m = -Number.MAX_VALUE;
+//   let res = 0;
+//   for (let [a,b] of ys) {
+//     m = Math.max(m, a);
+//     res += Math.max(0, b-m);
+//     m = Math.max(m, b);
+//   }
+//   return res;
+// }
+
+console.log(sumIntervals([
+  [1, 2],
+  [6, 10],
+  [11, 15]
+]));
+
+console.log(sumIntervals([
+  [1, 4],
+  [7, 10],
+  [3, 5]
+]));
+
+console.log(sumIntervals([
+  [1, 5],
+  [10, 20],
+  [1, 6],
+  [16, 19],
+  [5, 11]
+]));
+
+console.log(sumIntervals([
+  [0, 20],
+  [-100000000, 10],
+  [30, 40]
+]));
+
