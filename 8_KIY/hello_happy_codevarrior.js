ecb79595de125bd077b4c67b58da705fbc31b5c8
Dmitry Kanaev <dimmkan@mail.ru>
2023-06-30 08:15:33 +0300

    Hello Happy Codevarrior!

---
+++ 8_KIY/hello_happy_codevarrior.js
@@ -0,0 +1,23 @@
+/*
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+*/
+function Warrior(n){
+  this.n = n;  
+  this.name = function(name){
+    if(name) this.n = name;
+    return this.n;
+  }
+}
+  
+Warrior.prototype.toString = function(){
+    return "Hi! my name's "+this.name();
+}
