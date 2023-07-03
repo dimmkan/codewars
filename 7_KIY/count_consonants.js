afac7b58365f8cffdc0f8ac7e1462195e152179e
Dmitry Kanaev <dimmkan@mail.ru>
2023-07-03 10:21:35 +0300

    Count consonants

---
+++ 7_KIY/count_consonants.js
@@ -0,0 +1,7 @@
+/*
+Complete the function that takes a string of English-language text and returns the number of consonants in the string.
+
+Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+*/
+
+const consonantCount = s => (s.match(/[qzwsxdcrfvtgbyhnjmklp]/gi) || []).length;
