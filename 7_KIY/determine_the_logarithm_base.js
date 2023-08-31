7a16b715e6c5d023c083afe576b2b9029387f864
Dmitry Kanaev <dimmkan@mail.ru>
2023-08-31 10:26:25 +0300

    Determine the logarithm base

---
+++ 7_KIY/determine_the_logarithm_base.js
@@ -0,0 +1,10 @@
+/*
+Define a function that will receive a logarithm function, and returns the base of that logarithm.
+
+guessBase(ln) == e
+Base is a real number (not only integers) guaranteed to be less than 1e6.
+
+Have a fun time coding!
+*/
+
+const determineBase = logFunc => Math.pow(100, 1/logFunc(100));
