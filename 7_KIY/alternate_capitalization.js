086ad1dfb496c249728ab86393b47986f7157db5
Dmitriy Kanaev <dimmkan@mail.ru>
2023-11-17 09:05:39 +0300

    Alternate capitalization

---
+++ 7_KIY/alternate_capitalization.js
@@ -0,0 +1,17 @@
+/*
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+If you like this Kata, please try:
+
+Indexed capitalization
+
+Even-odd disparity
+*/
+
+const capitalize = (s) => [s.split('').map((v, i) => i % 2 == 0 ? v.toUpperCase() : v).join(''), s.split('').map((v, i) => i % 2 == 1 ? v.toUpperCase() : v).join('')];
