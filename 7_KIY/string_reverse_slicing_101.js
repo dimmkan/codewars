a5e7e16b9b6f90ac414be4f9a2f05f166e21d8e8
Dmitriy Kanaev <dimmkan@mail.ru>
2023-11-23 08:32:34 +0300

    String reverse slicing 101

---
+++ 7_KIY/string_reverse_slicing_101.js
@@ -0,0 +1,13 @@
+/*
+You'll be given a string of characters as an input. Complete the function that returns a list of strings: (a) in the reverse order of the original string, and (b) with each successive string starting one character further in from the end of the original string.
+
+Assume the original string is at least 3 characters long. Try to do this using slices and avoid converting the string to a list.
+
+Examples
+'123'   ==>  ['321', '21', '1']
+'abcde' ==>  ['edcba', 'dcba', 'cba', 'ba', 'a']
+*/
+
+const reverse = s => s.split('').reverse().join('');
+
+const reverseSlice = (str) => Array.from({ length: str.length }, (_, i) => reverse(str).slice(i, str.length));
