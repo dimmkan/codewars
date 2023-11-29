a5e6a42e384398dd09218383094d388fa5f0bbcc
Dmitriy Kanaev <dimmkan@mail.ru>
2023-11-29 08:24:10 +0300

    isEven? Bitwise series

---
+++ 7_KIY/is_even_bitwise_series.js
@@ -0,0 +1,16 @@
+/*
+Is the number even?
+If the numbers is even return true. If it's odd, return false.
+
+
+
+
+
+Oh yeah... the following symbols/commands have been disabled!
+
+use of %
+use of .even? in Ruby
+use of mod in Python
+*/
+
+const isEven = (n) => n - (Math.floor(n / 2) * 2) === 0 ? true : false; 
