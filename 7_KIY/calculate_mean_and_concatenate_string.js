7c1f2fce03970c49036c7c7b3516402d90764260
Dmitriy Kanaev <dimmkan@mail.ru>
2023-12-18 07:06:51 +0300

    Calculate mean and concatenate string

---
+++ 7_KIY/calculate_mean_and_concatenate_string.js
@@ -0,0 +1,17 @@
+/*
+You will be given an array which will include both integers and characters.
+
+Return an array of length 2 with a[0] representing the mean of the ten integers as a floating point number. There will always be 10 integers and 10 characters. Create a single string with the characters and return it as a[1] while maintaining the original order.
+
+lst = ['u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0']
+Here is an example of your return
+
+[3.6, "udiwstagwo"]
+In C# and Java the mean return is a double.
+*/
+
+function mean(lst) {
+    const str = lst.filter(v => Number.isNaN(+v));
+    const nmb = lst.filter(v => !Number.isNaN(+v)).map(v => ~~v);
+    return [(nmb.reduce((acc, item) => acc + item)) / nmb.length, str.reduce((acc, item) => acc + item)];
+}
