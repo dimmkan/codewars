f616dd19c4402a695eb145daf0af24c9adc7570f
Dmitriy Kanaev <dimmkan@mail.ru>
2024-01-17 08:23:24 +0300

    L2: Triple X

---
+++ 7_KIY/l2_triple_x.js
@@ -0,0 +1,14 @@
+/*
+Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
+
+"abraxxxas" → true
+"xoxotrololololololoxxx" → false
+"softX kitty, warm kitty, xxxxx" → true
+"softx kitty, warm kitty, xxxxx" → false
+Note :
+
+capital X's do not count as an occurrence of "x".
+if there are no "x"'s then return false
+*/
+
+const tripleX = str => /^[^x]*xxx/.test(str);
