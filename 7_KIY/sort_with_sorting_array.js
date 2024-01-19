9e028aa84983b862a47a2bfa1ea9fbb159865163
Dmitriy Kanaev <dimmkan@mail.ru>
2024-01-19 08:13:55 +0300

    Sort with a sorting array

---
+++ 7_KIY/sort_with_sorting_array.js
@@ -0,0 +1,14 @@
+/*
+Sort an array according to the indices in another array. It is guaranteed that the two arrays have the same size, and that the sorting array has all the required indices.
+
+initialArray = ['x', 'y', 'z'] sortingArray = [ 1, 2, 0]
+
+sort(initialArray, sortingArray) => ['z', 'x', 'y']
+
+sort(['z', 'x', 'y'], [0, 2, 1]) => ['z', 'y', 'x']
+*/
+
+const sort = (initialArray, sortingArray) => initialArray.reduce((acc, item, idx) => {
+    acc[sortingArray[idx]] = item;
+    return acc;
+}, []);
