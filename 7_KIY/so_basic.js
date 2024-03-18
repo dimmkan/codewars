049b51324fda87e9c0da9befbbffd6d623ba5d47
Dmitriy Kanaev <dimmkan@mail.ru>
2024-03-18 07:57:25 +0300

    So basic

---
+++ 7_KIY/so_basic.js
@@ -0,0 +1,10 @@
+/*
+Description:
+Summary: In this kata we want to communiate with a Mayan accountant.
+
+Scenario: You work for the IRS and have been sent the 1040 form for a local Mayan retailer. The Mayan apparently speaks perfect English but has accidentally filled out his Adjusted Gross Income (AGI) in Mayan.
+
+Task: Generate a javascript method that converts a base-20 value into its decimal equivalent.
+*/
+
+const convertBase20ToDecimal = (init) => isNaN(parseInt(init, 20)) ? -1 : parseInt(init, 20);
