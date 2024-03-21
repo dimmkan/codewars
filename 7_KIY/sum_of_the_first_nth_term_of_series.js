da8b3c67d17524f6157ddc9e49d0b67129934805
Dmitriy Kanaev <dimmkan@mail.ru>
2024-03-21 07:52:41 +0300

    Fix

--- 7_KIY/sum_of_the_first_nth_term_of_series.js
+++ 7_KIY/sum_of_the_first_nth_term_of_series.js
@@ -64,3 +64,10 @@ n
 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
 */
 
+function SeriesSum(n) {
+    for (var s = 0, i = 0; i < n; i++) {
+        s += 1 / (1 + i * 3);
+    }
+
+    return s.toFixed(2);
+}
